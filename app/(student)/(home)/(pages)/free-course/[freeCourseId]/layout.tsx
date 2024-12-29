fa4e841c53922f84dd84c0e07a6e3f31b9859bdc
Jheff4 <ogbevoenetinosa@gmail.com>
2024-12-30 00:46:14 +0100

    fixed tailwind

--- app/(student)/(home)/(pages)/free-course/[freeCourseId]/layout.tsx
+++ app/(student)/(home)/(pages)/free-course/[freeCourseId]/layout.tsx
@@ -1,18 +1,20 @@
 'use client';
 import React from "react";
-import FreeCourseRightSideBar
-    from "@/app/(student)/(home)/(pages)/free-course/[freeCourseId]/components/FreeCourseRightSideBar";
+import FreeCourseRightSideBar from "@/app/(student)/(home)/(pages)/free-course/[freeCourseId]/components/FreeCourseRightSideBar";
 
-export default function FreeCourseLayout(x: {
+type FreeCourseLayoutProps = {
     children: React.ReactNode;
     active?: boolean;
     setActive?: (active: boolean) => void;
-}) {
+};
 
+const FreeCourseLayout = ({ children, active, setActive }: FreeCourseLayoutProps) => {
     return (
         <section className="flex bg-[#FFF8F0]">
-            {x.children}
-            <FreeCourseRightSideBar active={x.active} setActive={x.setActive} />
+            {children}
+            <FreeCourseRightSideBar active={active} setActive={setActive} />
         </section>
     );
-}
+};
+
+export default FreeCourseLayout;
